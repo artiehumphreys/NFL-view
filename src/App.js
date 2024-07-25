1e4267da2e4d31e772f9f4a3353057c03b7b032f
artiehumphreys <artieh2003@gmail.com>
2024-07-25 16:32:21 -0400

    feat: NFL logo transition

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<div />} exact />
+        <Route path="/" element={<HomePage />} exact />
       </Routes>
     </Router>
   );
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,9 @@
+import React from "react";
 import { useNavigate } from "react-router-dom";
+import NFLLogo from "../components/NFLLogo";
 
-function HomePage() {}
+function HomePage() {
+  return <NFLLogo></NFLLogo>;
+}
 
 export default HomePage;
