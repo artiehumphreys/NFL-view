f293f27ecfe3a0352ebc77caee8e93cf49e54b4e
artiehumphreys <artieh2003@gmail.com>
2024-07-26 15:18:07 -0400

    feat: sqlite db creation

--- backend/main.go
+++ backend/main.go
@@ -3,10 +3,14 @@ package main
 import (
 	"fmt"
 
+	"github.com/artiehumphreys/NFL-view/database"
 	"github.com/artiehumphreys/NFL-view/pkg"
 )
 
 func main() {
+	db := database.InitDB("../NFLView.db")
+	defer db.Close()
+
 	filePath := "../alpha/NFLview_Database_2024.07.csv"
 	records := pkg.ReadCsvFile(filePath)
 
