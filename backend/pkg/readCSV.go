920578740b62088b42470a9b31a83f364aaaace9
artiehumphreys <artieh2003@gmail.com>
2024-07-26 13:48:14 -0400

    feat: csv to map method to convert data to maps for each row

--- backend/pkg/readCSV.go
+++ backend/pkg/readCSV.go
@@ -2,6 +2,7 @@ package pkg
 
 import (
 	"encoding/csv"
+	"io"
 	"log"
 	"os"
 )
@@ -14,6 +15,30 @@ func readCsvFile(filePath string) {
     defer f.Close()
 
     csvReader := csv.NewReader(f)
-
+    data := csvToMap(*csvReader)
     
+}
+
+func csvToMap(reader csv.Reader) []map[string]string {
+    rows := []map[string]string{}
+	var header []string
+    for {
+        row, err := reader.Read()
+        if err == io.EOF{
+            break
+        }
+        if err != nil {
+			log.Fatal(err)
+		}
+        if len(header) == 0 {
+			header = row
+        } else {
+            dict := map[string]string{}
+            for i := range header {
+				dict[header[i]] = row[i]
+			}
+            rows = append(rows, dict)
+        }
+    }
+    return rows
 }
