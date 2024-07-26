ede0ac8e96131142daf666666cc7d1860404ee47
artiehumphreys <artieh2003@gmail.com>
2024-07-26 10:44:05 -0400

    fix: eslint for react

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,42 @@
 import globals from "globals";
-import pluginJs from "@eslint/js";
+import reactPlugin from "eslint-plugin-react";
+import reactHooksPlugin from "eslint-plugin-react-hooks";
+import jsxA11yPlugin from "eslint-plugin-jsx-a11y";
+import importPlugin from "eslint-plugin-import";
+import babelParser from "@babel/eslint-parser";
 
 export default [
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
+  {
+    ignores: ["node_modules/**", "build/**", "dist/**"],
+  },
+  {
+    files: ["**/*.{js,jsx}"],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: "module",
+      globals: globals.browser,
+      parser: babelParser,
+      parserOptions: {
+        requireConfigFile: false,
+        babelOptions: {
+          presets: ["@babel/preset-react"],
+        },
+      },
+    },
+    plugins: {
+      react: reactPlugin,
+      "react-hooks": reactHooksPlugin,
+      "jsx-a11y": jsxA11yPlugin,
+      import: importPlugin,
+    },
+    rules: {
+      "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
+      "react/react-in-jsx-scope": "off",
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
 ];
