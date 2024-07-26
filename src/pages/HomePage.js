a84a73704f91759418382cbe32b3a4b78ce7909e
artiehumphreys <artieh2003@gmail.com>
2024-07-26 11:00:28 -0400

    feat: css styling for new home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,11 @@
+import styles from "../css/HomePage.module.css";
+
 function HomePage() {
-  return;
+  return (
+    <div className={styles.container}>
+      <span>hi</span>
+    </div>
+  );
 }
 
 export default HomePage;
--- src/pages/StartPage.js
+++ src/pages/StartPage.js
@@ -4,17 +4,15 @@ import styles from "../css/StartPage.module.css";
 
 function StartPage() {
   return (
-    <>
-      <div className={styles.container}>
-        <div className={styles.videoBackground}>
-          <video autoPlay loop muted className={styles.video}>
-            <source src="/NFL_Kickoff_2022.mp4" type="video/mp4" />
-            Your browser does not support the video tag.
-          </video>
-        </div>
-        <NFLLogo></NFLLogo>
+    <div className={styles.container}>
+      <div className={styles.videoBackground}>
+        <video autoPlay loop muted className={styles.video}>
+          <source src="/NFL_Kickoff_2022.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
       </div>
-    </>
+      <NFLLogo></NFLLogo>
+    </div>
   );
 }
 
