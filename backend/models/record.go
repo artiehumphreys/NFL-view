75c0ed19f4f6803ffae8a6ee9592357238df960d
artiehumphreys <artieh2003@gmail.com>
2024-07-26 14:03:47 -0400

    feat: record model

---
+++ backend/models/record.go
@@ -0,0 +1,14 @@
+package models
+
+type Record struct {
+	ID           int
+	PlayID       int
+	NFLPlayerID  int
+	Type         string
+	GamePosition string
+	Team         string
+	JerseyNumber string
+	FirstName    string
+	LastName     string
+	Quality      string
+}
