6997d4e06c654b50ca2bb1ac86ceb0d688749f95
artiehumphreys <artieh2003@gmail.com>
2024-07-29 14:18:02 -0400

    feat: menu bar for settings

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -49,11 +49,18 @@ function HomePage() {
           </section>
         </main>
         <button
-          className="absolute top-4 right-4 text-blue-500 p-2 rounded"
+          className={` ${styles.button} button absolute top-4 right-4 text-blue-500 p-2 rounded`}
           onClick={toggleMenu}
         >
           {isMenuOpen ? <FaTimes /> : <FaBars />}
         </button>
+        <aside
+          className={`w-64 bg-gray-100 p-4 flex flex-col absolute right-0 top-0 bottom-0 transition-transform transform ${
+            isMenuOpen ? "-translate-x-0" : "translate-x-full"
+          }`}
+        >
+          <h2 className="text-2xl font-bold mb-4">Menu</h2>
+        </aside>
       </div>
     </div>
   );
