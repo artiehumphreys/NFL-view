0e0fe4442b42ded772cc2179683b03c9f4f6faec
artiehumphreys <artieh2003@gmail.com>
2024-07-29 13:30:42 -0400

    feat: game side bar for showing list of games

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,5 @@
-import styles from "../css/HomePage.module.css";
 import { FaSearch } from "react-icons/fa";
+import styles from "../css/HomePage.module.css";
 
 function HomePage() {
   return (
@@ -7,9 +7,12 @@ function HomePage() {
       <header className="bg-blue-500 text-white text-center py-4">
         <h1 className="text-5xl font-bold">NFL View</h1>
       </header>
-      <div className="flex flex-1">
-        <main className="flex-1 p-3 flex flex-col items-center">
-          <section className="my-1 content-center">
+      <div className="flex flex-1 relative">
+        <aside className="w-64 bg-gray-100 p-4 absolute left-0 top-0 bottom-0">
+          <h2 className="text-2xl font-bold mb-4">Games</h2>
+        </aside>
+        <main className="flex-1 p-6 flex flex-col items-center">
+          <section className="my-1 w-max">
             <div className="flex items-center mb-4">
               <input
                 type="text"
@@ -20,7 +23,7 @@ function HomePage() {
                 <FaSearch className="w-6 h-6" />
               </button>
             </div>
-            <div className="flex gap-2 flex-row items-center">
+            <div className="flex gap-2 flex-row items-center mb-4">
               <h2 className="text-xl font-semibold">Search by</h2>
               <button className="bg-gray-200 text-gray-700 py-1 px-3 rounded">
                 Tag 1
