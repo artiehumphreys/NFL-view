679fd70ae1a12d1a9a9e5624b644c5defe3900a2
artiehumphreys <artieh2003@gmail.com>
2024-07-31 14:44:33 -0400

    feat: functioning dropdowns for games and margin fixes

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -8,6 +8,11 @@ function HomePage() {
     setIsMenuOpen(!isMenuOpen);
   };
 
+  const [visibleGameIndex, setVisibleGameIndex] = useState(null);
+  const toggleVisibility = (index) => {
+    setVisibleGameIndex(visibleGameIndex === index ? null : index);
+  };
+
   const [searchTags, setSearchTags] = useState([]);
   const [displayInfo, setDisplayInfo] = useState([]);
   const [gameInfo, setGameInfo] = useState([]);
@@ -34,24 +39,47 @@ function HomePage() {
         <h1 className="text-5xl font-bold">NFL View</h1>
       </header>
       <div className="flex flex-1 relative overflow-auto">
-        <aside className="w-64 bg-gray-100 px-3 py-4 absolute left-0 top-0 bottom-0 flex flex-col w-max">
+        <aside className="w-64 bg-gray-100 px-3 py-4 absolute left-0 top-0 bottom-0 flex flex-col flex-1">
           <div className="flex-shrink-0 mb-4">
             <h2 className="text-2xl font-bold">Games</h2>
           </div>
           <div className="flex-1 overflow-auto">
             {gameInfo.map((game, index) => (
               <div key={index}>
-                <h2 className="font-semibold">{game.game}</h2>
-                <ul className="gap-2 py-2 flex flex-col text-left">
-                  {game.events.map((event, idx) => (
-                    <button
-                      key={idx}
-                      className="bg-gray-200 text-gray-700 py-2 px-3 my-1 rounded hover:bg-gray-300 text-left"
-                    >
-                      {event}
-                    </button>
-                  ))}
-                </ul>
+                <h2
+                  onClick={() => toggleVisibility(index)}
+                  className="font-medium hs-dropdown-toggle py-2 px-1 inline-flex items-center gap-x-2 font-large rounded-lg border-gray-200 hover:bg-gray-50 focus:bg-gray-50 cursor-pointer w-full"
+                >
+                  {game.game}
+                  <svg
+                    className={`hs-dropdown-open:rotate-180 size-4 ${
+                      visibleGameIndex === index ? "rotate-180" : ""
+                    }`}
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  >
+                    <path d="m6 9 6 6 6-6" />
+                  </svg>
+                </h2>
+                {visibleGameIndex === index && (
+                  <ul className="gap-2 py-2 flex flex-col text-left">
+                    {game.events.map((event, idx) => (
+                      <button
+                        key={idx}
+                        className="bg-gray-200 text-gray-700 py-2 mx-2 px-2 my-1 rounded hover:bg-gray-300 text-left"
+                      >
+                        {event}
+                      </button>
+                    ))}
+                  </ul>
+                )}
               </div>
             ))}
           </div>
