b8b4c93087ab6f705a94afae52af4432baac5a7a
artiehumphreys <artieh2003@gmail.com>
2024-08-01 10:46:50 -0400

    feat: component for header

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import { IoMdArrowRoundBack } from "react-icons/io";
+import { useNavigate } from "react-router-dom";
+
+function Header() {
+  const navigate = useNavigate();
+  return (
+    <header className="bg-blue-500 text-white text-center py-4 items-center flex justify-between">
+      <IoMdArrowRoundBack
+        size={40}
+        className="ml-2 cursor-pointer"
+        onClick={() => navigate("/")}
+      ></IoMdArrowRoundBack>
+      <h1 className="text-5xl font-bold flex-grow text-center">NFL View</h1>
+      <div className="w-10"></div>
+    </header>
+  );
+}
+
+export default Header;
